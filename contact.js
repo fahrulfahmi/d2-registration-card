1d544bdff1600d28e5076c799a82dacfddb57a36
fahrulfahmi <vanjoelarrazie@gmail.com>
2022-11-16 16:45:42 +0700

    Days 2 - Registration Page

---
+++ contact.js
@@ -0,0 +1,11 @@
+// function submitData() {
+// 	let name = document.getElementById(name).value
+// 	let email = document.getElementById(email).value
+// 	let phonenumber = document.getElementById(phonenumber).value
+// 	let subject = document.getElementById(subject).value
+// 	let message = document.getElementById(message).value
+// 	console.log(name, email, phonenumber, subject, message)
+
+// 	let link = document.createElement( `a` )
+// 	link.href = `mailto:${emailReceiver}? subject=$ {ubject}& body=hallo nama saya ${name}`
+// }
